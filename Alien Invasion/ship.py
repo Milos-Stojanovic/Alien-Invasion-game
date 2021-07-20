b3b0610e33a7ffba5ad78cb12a356e06d9b0b03b
Miloš Stojanović <78445216+Milos-Stojanovic@users.noreply.github.com>
2021-07-20 18:20:09 +0200

    Update ship.py

--- Alien Invasion/ship.py
+++ Alien Invasion/ship.py
@@ -8,6 +8,7 @@ class Ship:
 
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get it's rect
         self.image = pygame.image.load("C:/Users/Marko/Desktop/Miloš/PYTHON/Alien Invasion/images/ship.bmp")
@@ -15,6 +16,25 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        """Update the ship's position based on movement flags."""
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
 
     def blitme(self):
