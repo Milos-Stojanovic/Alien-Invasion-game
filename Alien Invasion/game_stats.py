5abb09b93b5245870cd2beab97a6a3177a36f145
Miloš Stojanović <78445216+Milos-Stojanovic@users.noreply.github.com>
2021-07-22 01:25:57 +0200

    Update game_stats.py

--- Alien Invasion/game_stats.py
+++ Alien Invasion/game_stats.py
@@ -1,3 +1,5 @@
+import json
+
 class GameStats:
     """Track statistics for Alien Invasion."""
 
@@ -7,11 +9,33 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False
+
+        # High score (it should never be reset)
+        self._read_high_score()
 
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
 
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
+
+
+    def _read_high_score(self):
+        """Read the current high score from a file."""
+
+        try:
+            with open('C:/Users/Marko/Desktop/Miloš/PYTHON/Alien Invasion/high_score.json') as f:
+                hs = json.load(f)
+
+        except ValueError or FileNotFoundError:
+            hs = None
+        
+
+        if hs:
+            self.high_score = hs
+        else:
+            self.high_score = 0
